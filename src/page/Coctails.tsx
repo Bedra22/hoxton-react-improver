da01ea6fe6dea32e0803a78d6fe861cbcdc609d1
Bedra Kraja <bedra.kraja22@gmail.com>
2022-08-14 13:48:15 +0200

    vahzhdimmmmmm

--- src/page/Coctails.tsx
+++ src/page/Coctails.tsx
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom"
 
 export function Coctails() {
     const [coctaile, setCoctaile] = useState([])
+    const [searchcoctailes, setSearchCoctailes] = useState('')
 
     useEffect(() => {
         fetch('http://localhost:4000/coctails')
             .then(resp => resp.json())
             .then(CoctailFromServer => setCoctaile(CoctailFromServer))
     }, [])
+
+    const filtercocktailes = coctaile.filter(filteredcoctlaies => filteredcoctlaies.name.toLowerCase().includes(searchcoctailes))
     return (
         <div>
             <div className="recepies-text">
@@ -19,10 +22,22 @@ export function Coctails() {
                     “If life gives you lemon, make lemonade…and find someone whose life gave them vodka.”
                 </p>
             </div>
+            <div className="search-recepies">
+                <div className="search-recepies-input-div">
+                    <input
+                        type="text"
+                        name="search"
+                        placeholder="Search here..."
+                        onChange={event =>
+                            setSearchCoctailes(event.target.value)} />
+                    <button>🔍</button>
+
+                </div>
+            </div>
             <div className="recepies-loop">
                 <div className="recepies-loop-div">
                     <ul>
-                        {coctaile.map(taile => (
+                        {filtercocktailes.map(taile => (
                             <li>
                                 <Link to={`/coctails/${taile.id}`} style={{ textDecoration: 'none' }}>
                                     <div className="one-recipe">
--- src/page/Recepies.tsx
+++ src/page/Recepies.tsx
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom"
 
 export function Recepies() {
     const [recepies, setRecepies] = useState([])
+    const [searchrecepie, setSearchRecepie] = useState('')
+
 
     useEffect(() => {
         fetch('http://localhost:4000/recepies')
             .then(resp => resp.json())
             .then(RecepiesFromServer => setRecepies(RecepiesFromServer))
     }, [])
+
+    const filterrecepies = recepies.filter(filteredrecepies => filteredrecepies.name.toLowerCase().includes(searchrecepie))
+
     return (
         <div>
             <div className="recepies-text">
@@ -19,10 +24,22 @@ export function Recepies() {
                     We’ve organized these recipes every way we could think of so you don't have to! Dietary restrictions, weeknight dinners, meal prep recipes, some of our most tried-and-true… no matter how you browse, we’re sure you’ll find just what you were looking for.
                 </p>
             </div>
+            <div className="search-recepies">
+                <div className="search-recepies-input-div">
+                    <input
+                        type="text"
+                        name="search"
+                        placeholder="Search here..."
+                        onChange={event => {
+                            setSearchRecepie(event.target.value)
+                        }} />
+                    <button>🔍</button>
+                </div>
+            </div>
             <div className="recepies-loop">
                 <div className="recepies-loop-div">
                     <ul>
-                        {recepies.map(OneRecipe => (
+                        {filterrecepies.map(OneRecipe => (
                             <li>
                                 <Link to={`/recepies/${OneRecipe.id}`} style={{ textDecoration: 'none' }}>
                                     <div className="one-recipe">
